5e543eb622e1d18312a096e95364ad4f5a9edde6
Daaviid30 <davidmartinc30@gmail.com>
2023-10-24 14:18:02 +0200

    comentar papers

--- app/papers.py
+++ app/papers.py
@@ -7,9 +7,11 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.ciphers.aead import ChaCha20Poly1305
 
 
-
+# Guardamos el paper introducido, cifrandolo en el proceso
 def guardar_paper(titulo, cuerpo):
+    # Obtenemos los valores de contraseña y DNI para  insertar el paper en el usuario que corresponde
     password_key, identificador = pasar_password(), pasar_identificador()
+    # Encriptamos con un MAC la contraseña en claro para crear una clave de encriptación
     salt = os.urandom(16)
     kdf = PBKDF2HMAC(
         algorithm=hashes.SHA256(),
@@ -19,6 +21,7 @@ def guardar_paper(titulo, cuerpo):
 
     key = kdf.derive(password_key.encode('utf-8'))
 
+    # Creamos un nonce y un aad necesarios para encriptar el cuerpo, usamos como aad el titulo del paper
     data = cuerpo.encode('utf-8')
     aad = titulo.encode('utf-8')
     chacha = ChaCha20Poly1305(key)
@@ -26,6 +29,7 @@ def guardar_paper(titulo, cuerpo):
 
     encrypted_data = chacha.encrypt(nonce, data, aad)
 
+    # Introducimos en la base de datos los datos del paper que necesitamos conservar
     try:
         insertar_datos = "INSERT INTO papers (dni, titulo, cuerpo, salt, nonce) VALUES (%s, %s, %s, %s, %s)"
         values = (identificador, titulo, encrypted_data, salt, nonce)
@@ -38,6 +42,8 @@ def guardar_paper(titulo, cuerpo):
         mensaje = "No se pudo guardar el paper"
         return mensaje, "error"
 
+# Creamos una funcion que haga una consulta  a la bbdd y devuelva una lista con todos los titulos de los papers
+# de un usuario
 def listar_papers():
     identificador = pasar_identificador()
     consulta = "SELECT titulo FROM papers WHERE dni = %s"
@@ -47,6 +53,7 @@ def listar_papers():
 
     return resultados
 
+# Creamos una funcion que recupere el cuerpo para mostrarlo descifrado, si se ha modificado el texto dará error al mostrar
 def recuperar_cuerpo(titulo):
     identificador = pasar_identificador()
     password = pasar_password()
